dfc2d263764e9bfb6ea9b86d85e366e37371f0ae
ludmilagudino <ludmitrini@gmail.com>
2022-12-28 12:40:26 -0300

    dom coder

--- DESAFIOS/3/main.js
+++ DESAFIOS/3/main.js
@@ -1,7 +1,18 @@
-let frase = prompt('Ingrese la frase: ')
+// let frase = prompt('Ingrese la frase: ')
 
-function reversa (frase){
-    return frase.split('').reverse().join("").split(" ").reverse().join(" ");
+// function reversa (frase){
+//     return frase.split('').reverse().join("").split(" ").reverse().join(" ");
+// }
+
+// console.log(reversa(frase));
+
+function BinarioADecimal(num) {
+    let sum = 0;
+    
+    for (let i = 0; i < num.length; i++) {
+       sum += num[i] * 2 ** (num.length -1 -i);
+    }
+    return sum;
 }
 
-console.log(reversa(frase));
+console.log(BinarioADecimal("1011"));
